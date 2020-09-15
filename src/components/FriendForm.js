3d46ff0e8f6ea7c5bcfdf867cff76ee79ec73bcd
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-15 19:12:00 +0200

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -57,7 +57,9 @@ export default function FriendForm(props) {
           {/* 🔥 STEP 5 - Make dropdown for role. */}
           <select name='role' value={values.role} onChange={onChange}>
               <option value="">-- select role --</option>
-              <option value="">-- select role --</option>
+              <option value="instructor">Instructor</option>
+              <option value="student">Student</option>
+              <option value="tl">Team Lead</option>
           </select>
         </label>
 
