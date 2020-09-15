347d65784414e3c21a655bcefb93431f806c3515
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-15 19:56:19 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -41,7 +41,7 @@ export default function App() {
     axios.post('fakeapi.com', newFriend)
       .then(res => {
         // setFriends(friends.concat(res.data))
-        setFriends([ res.data, ...friends])
+        setFriends([ newFriend, ...friends])
         setFormValues(initialFormValues)
       })
       .catch(err => {
