08eb3feb411914578a7d8e586b4b4fcd53bef671
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-15 19:01:26 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import axios from '../axios'
 // 👉 the shape of the state that drives the form
 const initialFormValues = {
   ///// TEXT INPUTS /////
-  username: 'Trevor',
+  username: '',
   email: '',
   ///// DROPDOWN /////
   role: '',
